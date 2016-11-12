5e5889ce3f5fc82031787ccd8e39aa4e1b705c03
Fred <delon.frederic.pro.job@gmail.com>
2016-11-12 19:27:45 +0100

    end ( all step clear )

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 	"use strict";
 
-
 	var me = {
-		colorBody:null,
 		red:null,
 		green:null,
 		blue:null,
+		alpha:1,
 
 		init: function(){
 			me.range();
 			me.changeBodyColor();
+			me.showColorhexa();
 		},
 		range: function(){
 			$('.range').text('0');
@@ -23,21 +23,26 @@ $(document).ready(function(){
 			me.red = $('#red').val();
 			me.green = $('#green').val();
 			me.blue = $('#blue').val();
+			me.alpha = $('#alpha').val();
 			$('input').on('change', function(){
-				$('body').css('background-color',"rgb("+ me.red +","+me.green+","+me.blue+")");
-				// console.log(red,green,blue);
-				me.showColorNumber();
+				$('body').css('background-color',"rgba("+ me.red +","+me.green+","+me.blue+","+me.alpha+")");
 				me.changeBodyColor();
+				me.showColorNumber();
+				me.showColorhexa();
 			});
 
 		},
 		showColorNumber: function(){
-			$('#colorValue').html(" rgb ,"+me.red+","+me.green+","+me.blue+", to ,");
+			$('#colorValue').val(""+me.red+","+me.green+","+me.blue+"");
+			$('#colorValue').text(""+me.red+","+me.green+","+me.blue+"");
 		},
-		// showColorhexa: function(){
-
-		// },
-
+		showColorhexa: function(){
+			var hex = me.rgb2hex( $('#colorValue').val()); 
+			$('#result').val( hex );
+		},
+		rgb2hex:function (){
+			return "#" + ((1 << 24) + (me.red << 16) + (me.green << 8) + (me.blue << 0)).toString(16).slice(1);
+		}
 	}
 	me.init();
 });
